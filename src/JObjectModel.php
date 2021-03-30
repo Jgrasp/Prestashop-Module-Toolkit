3263348c04990721765ad7d7be02f0356631a839
Jgrasp <briaisq@hotmail.fr>
2021-03-30 16:03:48 +0200

    Fix table association in JObjectModel.php

--- src/JObjectModel.php
+++ src/JObjectModel.php
@@ -9,7 +9,7 @@ class JObjectModel extends \ObjectModel
     public function __construct($id = null, $id_lang = null, $id_shop = null)
     {
         if (isset(static::$definition['multilang_shop']) && static::$definition['multilang_shop'] === true) {
-            Shop::addTableAssociation(self::$definition['table'], array('type' => 'shop'));
+            Shop::addTableAssociation(static::$definition['multilang_shop'], array('type' => 'shop'));
         }
 
         parent::__construct($id, $id_lang, $id_shop);
