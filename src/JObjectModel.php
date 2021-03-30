a41087dca00cfb28d82c6a94286349669e8eda6a
Jgrasp <briaisq@hotmail.fr>
2021-03-30 16:02:02 +0200

    Add JObjectModel.php

---
+++ src/JObjectModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Jgrasp\Toolkit;
+
+use Shop;
+
+class JObjectModel extends \ObjectModel
+{
+    public function __construct($id = null, $id_lang = null, $id_shop = null)
+    {
+        if (isset(static::$definition['multilang_shop']) && static::$definition['multilang_shop'] === true) {
+            Shop::addTableAssociation(self::$definition['table'], array('type' => 'shop'));
+        }
+
+        parent::__construct($id, $id_lang, $id_shop);
+    }
+}
