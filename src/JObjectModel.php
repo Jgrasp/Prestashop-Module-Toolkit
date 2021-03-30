65c5d58c5bb1af9aa7fc9197b1ef00762b8b7ad8
Jgrasp <briaisq@hotmail.fr>
2021-03-30 16:07:49 +0200

    Add restriction on shop association condition to improve performance

--- src/JObjectModel.php
+++ src/JObjectModel.php
@@ -8,7 +8,7 @@ class JObjectModel extends \ObjectModel
 {
     public function __construct($id = null, $id_lang = null, $id_shop = null)
     {
-        if (isset(static::$definition['multilang_shop']) && static::$definition['multilang_shop'] === true) {
+        if (isset(static::$definition['multilang_shop']) && static::$definition['multilang_shop'] === true && Shop::isTableAssociated($this->def['table'])) {
             Shop::addTableAssociation(static::$definition['table'], array('type' => 'shop'));
         }
 
